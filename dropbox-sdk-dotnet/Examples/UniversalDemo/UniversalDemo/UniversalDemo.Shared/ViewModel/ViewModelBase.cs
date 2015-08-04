936bffad2b774833d3347441051ce266130470dd
Ken Elkabany <kelkabany@dropbox.com>
2015-08-03 18:09:55 -0700

    Backport from public repo.

---
+++ dropbox-sdk-dotnet/Examples/UniversalDemo/UniversalDemo/UniversalDemo.Shared/ViewModel/ViewModelBase.cs
@@ -0,0 +1,54 @@
+﻿namespace UniversalDemo.ViewModel
+{
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+    using Windows.UI.Core;
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// Base class for view model objects.
+    /// </summary>
+    public class ViewModelBase : DependencyObject, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when the value of a property has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets the application instance.
+        /// </summary>
+        public App App
+        {
+            get
+            {
+                return (App)Application.Current;
+            }
+        }
+
+        /// <summary>
+        /// Invokes an action on the UI thread asynchronously.
+        /// </summary>
+        /// <param name="action">The action to invoke.</param>
+        /// <returns>An asynchronous task.</returns>
+        protected async Task InvokeAsync(DispatchedHandler action)
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, action);
+        }
+
+        /// <summary>
+        /// Notifies observers that a property value has changed.
+        /// </summary>
+        /// <param name="propName">Name of the property that has changed, if this
+        /// is the empty string, then all properties are assumed to have changed.</param>
+        protected void NotifyPropertyChanged(string propName = "")
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
---
+++ dropbox-sdk-dotnet/Examples/UniversalDemo/UniversalDemo/UniversalDemo.Windows/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace UniversalDemo
+{
+    using System;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+
+    /// <summary>
+    /// The main application page.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// The swipe manager, this manages the swipe manipulation.
+        /// </summary>
+        private SwipeManager swipeManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var imageSet = new ViewModel.AppImageSet();
+            this.DataContext = imageSet;
+            this.swipeManager = new SwipeManager(this.ImageHolder, imageSet);
+
+            this.Loaded += this.OnLoaded;
+            this.SizeChanged += this.OnLoaded;
+        }
+
+
+        /// <summary>
+        /// Called when the app is loaded or when the size changes, this keeps the
+        /// previous and next images off screen.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var nextTransform = (TranslateTransform)this.NextHolder.RenderTransform;
+            nextTransform.X = this.RootGrid.ActualWidth;
+
+            var prevTransform = (TranslateTransform)this.PrevHolder.RenderTransform;
+            prevTransform.X = -this.RootGrid.ActualWidth;
+        }
+    }
+}
