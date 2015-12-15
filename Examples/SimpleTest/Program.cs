2959292734838963d7da0c7b70763da4d8eac013
Qiming Yuan <yuanqiming@gmail.com>
2015-12-15 11:51:20 -0800

    Added more example code to SimpleTest project.

--- Examples/SimpleTest/Program.cs
+++ Examples/SimpleTest/Program.cs
@@ -1,6 +1,7 @@
 namespace SimpleTest
 {
     using System;
+    using System.IO;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
@@ -63,6 +64,10 @@ namespace SimpleTest
                 {
                     await Download(path, firstFile.AsFile);
                 }
+
+                await Upload(path, "Test.txt", "This is a text file");
+
+                await ChunkUpload(path, "Binary");
             }
             catch (HttpException e)
             {
@@ -252,6 +257,82 @@ namespace SimpleTest
                 Console.WriteLine(await response.GetContentAsStringAsync());
                 Console.WriteLine("------------------------------");
             }
+        }
+
+        /// <summary>
+        /// Uploads given content to a file in Dropbox.
+        /// </summary>
+        /// <param name="folder">The folder to upload the file.</param>
+        /// <param name="fileName">The name of the file.</param>
+        /// <param name="fileContent">The file content.</param>
+        /// <returns></returns>
+        private async Task Upload(string folder, string fileName, string fileContent)
+        {
+            Console.WriteLine("Upload file...");
+
+            using (var stream = new MemoryStream(System.Text.UTF8Encoding.UTF8.GetBytes(fileContent)))
+            {
+                var response = await this.client.Files.UploadAsync(folder + "/" + fileName, WriteMode.Overwrite.Instance, body: stream);
+
+                Console.WriteLine("Uploaded Id {0} Rev {1}", response.Id, response.Rev);
+            }
+        }
+
+        /// <summary>
+        /// Uploads a big file in chunk. The is very helpful for uploading large file in slow network condition
+        /// and also enable capability to track upload progerss.
+        /// </summary>
+        /// <param name="folder">The folder to upload the file.</param>
+        /// <param name="fileName">The name of the file.</param>
+        /// <returns></returns>
+        private async Task ChunkUpload(string folder, string fileName)
+        {
+            Console.WriteLine("Chunk upload file...");
+            // Chunk size is 128KB.
+            const int chunkSize = 128 * 1024;
+
+            // Create a random file of 1MB in size.
+            var fileContent = new byte[1024 * 1024];        
+            new Random().NextBytes(fileContent);
+
+            using (var stream = new MemoryStream(fileContent))
+            {
+                int numChunks = (int)Math.Ceiling((double)stream.Length / chunkSize);
+
+                byte[] buffer = new byte[chunkSize];
+                string sessionId = null;
+
+                for (var idx = 0; idx < numChunks; idx++)
+                {
+                    Console.WriteLine("Start uploading chunk {0}", idx);
+                    var byteRead = stream.Read(buffer, 0, chunkSize);
+
+                    using (MemoryStream memStream = new MemoryStream(buffer, 0, byteRead))
+                    {
+                        if (idx == 0)
+                        {
+                            var result = await this.client.Files.UploadSessionStartAsync(memStream);
+                            sessionId = result.SessionId;
+                        }
+
+                        else
+                        {
+                            UploadSessionCursor cursor = new UploadSessionCursor(sessionId, (ulong)(chunkSize * idx));
+
+                            if (idx == numChunks - 1)
+                            {
+                                await this.client.Files.UploadSessionFinishAsync(cursor, new CommitInfo(folder + "/" + fileName), memStream);
+                            }
+
+                            else
+                            {
+                                await this.client.Files.UploadSessionAppendAsync(cursor, memStream);
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
