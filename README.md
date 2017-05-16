44d2b2e77fbcebcf165e9c3f5e75f1757b1ad250
Qiming Yuan <qiming@dropbox.com>
2017-05-16 15:40:42 -0700

    Update instruction and generation script.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-Private Dropbox .NET SDK
+Dropbox .NET SDK
 ========================
 
-This repository is used to generate the [public Dropbox .NET SDK]
-(https://github.com/dropbox/dropbox-sdk-dotnet).
-
+This repository is used to generate the Dropbox .NET SDK
 This repository contains no auto-generated code.
 
 Basic Setup
@@ -12,12 +10,11 @@ Basic Setup
   - Visual Studio 2013 or above.
   - Python 2.7 or above.
   - Sandcastle Help File Builder installed (https://github.com/EWSoftware/SHFB/releases).
-2. Clone the public and private SDK repo into `~/src`.
 
-3. In the private SDK repo, run ``git submodule init`` followed by a
+2. Run ``git submodule init`` followed by a
    ``git submodule update`` to pull in the ``spec`` and ``stone`` sub repos.
 
-4. In the private SDK repo, run
+4. Run
    ```
    cd stone
    python setup.py install
@@ -33,43 +30,28 @@ Updating the SDK for a new spec
    $ git pull
    ```
 
-2. Use the `generate.py` script with the path to the local check out of the public:
+2. Run `generate.py` script to generatedi class for latest data types.
 
    ```
-   $ python generate.py -h
-   $ python generate.py ../dropbox-sdk-dotnet
+   $ python generate.py
    ```
 
-3. From the public repo, open up the `Dropbox.Api.sln` in Visual Studio and run
+3. Open up the `Dropbox.Api.sln` in Visual Studio and run
    the included examples as a sanity check.
 
-4. Commit the update to the spec sub repo (from step 1) to the private SDK repo.
-   Do this from the root of the private SDK repo:
-
-   ```
-    $ git commit -a -m "Updated spec"
-    $ git push
-   ```
-
-5. Commit and push the public repo.
-
-
-Publishing a new SDK (This needs to be done on Windows)
+Create nuget package (This needs to be done on Windows)
 --------------------
 1. From the private repo, edit `dropbox-sdk-dotnet/Dropbox.Api/Dropbox.Api.nuspec` and update release note.
 2. Edit buildall.ps1 and update major version and release version.
 3. In Visual Studio Developer Command Prompt run
    ```
-   powershell -ExecutionPolicy Bypass -File buildall.ps1 -testSettings:<PATH_TO_TEST_SETTINGS> -signKeyPath:<PATH_TO_SIGNING_KEY>
+   powershell -ExecutionPolicy Bypass -File buildall.ps1 -testSettings:<PATH_TO_TEST_SETTINGS> 
    ```
    A .nukpg file will be generated in `Dropbox.Api` directory.
-3. Go to nuget.org to publish the package. Please ask around for the account credentials.
 
 Generating Docs
 ---------------
 1. In Visual Studio Developer Command Prompt, run
    ```
-   powershell -ExecutionPolicy Bypass -File buildall.ps1 -testSettings:<PATH_TO_TEST_SETTINGS> -signKeyPath:<PATH_TO_SIGNING_KEY> -doc:True
+   powershell -ExecutionPolicy Bypass -File buildall.ps1 -testSettings:<PATH_TO_TEST_SETTINGS> -doc:True
    ```
-2. Push generated code in doc/Help to public homepage branch.
-
--- generate.py
+++ generate.py
@@ -1,108 +1,30 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
 
-import argparse
-import errno
 import glob
 import os
 import shutil
 import subprocess
-import sys
 
-from distutils.dir_util import copy_tree
-
-REMOTE_ORIGIN_URLS = (
-    'git@github.com:dropbox/dropbox-sdk-dotnet.git',
-    'ssh://git@github.com/dropbox/dropbox-sdk-dotnet.git',
-    'https://github.com/dropbox/dropbox-sdk-dotnet.git',
-)
-
-cmdline_desc = """\
-Updates a local checkout of the public .NET SDK on GitHub (dropbox-sdk-dotnet).
-"""
-
-_cmdline_parser = argparse.ArgumentParser(description=cmdline_desc)
-_cmdline_parser.add_argument(
-    '-v',
-    '--verbose',
-    action='store_true',
-    help='Print debugging statements.',
-)
-_cmdline_parser.add_argument(
-    'repo_path',
-    type=str,
-    help='Path to a checkout of the dropbox-sdk-dotnet repo.',
-)
-
-def check_remote_origin_url(repo_path, accepted_remote_origin_urls):
-    """
-    If the remote origin url of the specified repo is not found in
-    accepted_remote_origin_urls, prints an error to stderr and exits.
-
-    Args:
-        repo_path (str): Path to a git repository.
-        accepted_remote_origin_urls (List[str])
-    """
-    stdout = subprocess.check_output(
-        ['git', 'config', '--local', '--get', 'remote.origin.url'],
-        cwd=repo_path)
-    remote_url = stdout.rstrip()
-    if remote_url not in accepted_remote_origin_urls:
-        print('error: The repo (%s) has an unexpected remote origin url:' %
-              repo_path, file=sys.stderr)
-        print('    %s' % remote_url)
-        print('Expected either:')
-        for url in accepted_remote_origin_urls:
-            print('    %s' % url, file=sys.stderr)
-        sys.exit(1)
-
-def clear_repo(repo_path):
-    for path in glob.glob(os.path.join(repo_path, '*')):
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-        else:
-            os.remove(path)
 
 def main():
     """The entry point for the program."""
-
-    args = _cmdline_parser.parse_args()
-    repo_path = args.repo_path
-    verbose = args.verbose
-
-    # Sanity check repository path.
-    if not os.path.exists(repo_path):
-        print('error: The repo folder (%s) does not exist.' % repo_path,
-              file=sys.stderr)
-        sys.exit(1)
-    if not os.path.isdir(repo_path):
-        print('error: The repo path (%s) must be a folder.' % repo_path,
-              file=sys.stderr)
-        sys.exit(1)
-
-    # Check that repo path points to the top-level of the repo
-    if not os.path.exists(os.path.join(repo_path, '.git')):
-        print('error: The repo folder (%s) is not the top-level of the '
-              'public SDK repo.' % repo_path)
-        sys.exit(1)
-
-    check_remote_origin_url(repo_path, REMOTE_ORIGIN_URLS)
-
-    clear_repo(repo_path)
-
-    if verbose:
-        print('Copying from private repo')
-    copy_tree('dropbox-sdk-dotnet', repo_path)
-
-    if verbose:
-        print('Generating code')
-
-    shutil.rmtree(os.path.join(repo_path, 'Dropbox.Api/Generated'))
-    subprocess.check_output(
-        (['python', '-m', 'stone.cli', '--filter-by-route-attr', 'alpah_group=null', '-a:all', 'generator/csharp.stoneg.py'] +
-         [os.path.join(repo_path, 'Dropbox.Api')] + glob.glob('spec/*.stone')),
-	 env={'PYTHONPATH': 'stone'})
+    
+    repo_path = 'dropbox-sdk-dotnet'
+    print('Generating code')
+    try:
+        for path in glob.glob(os.path.join(repo_path, 'Dropbox.Api/*.csproj')):
+            os.remove(path)
+        shutil.rmtree(os.path.join(repo_path, 'Dropbox.Api', 'Generated'))
+    except OSError:
+        pass
+    try:
+        subprocess.check_output(
+            (['python', '-m', 'stone.cli', '--filter-by-route-attr', 'alpah_group=null', '-a:all', 'generator/csharp.stoneg.py'] +
+             [os.path.join(repo_path, 'Dropbox.Api')] + glob.glob('spec/*.stone')),
+            env={'PYTHONPATH': 'stone'})
+    except subprocess.CalledProcessError as e:
+        print(e.output)
 
 if __name__ == '__main__':
     main()
-
