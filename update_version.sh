01531b1df799b909775e306754ad16d5132f9af6
Brad Rogers <brad12ogers@gmail.com>
2020-04-30 11:05:29 -0700

    Add update version script

---
+++ update_version.sh
@@ -0,0 +1,14 @@
+if [ -z $1 ]; then
+    echo "error: $0 needs a version number as argument.";
+    exit 1
+else
+    set -ex
+    NEW_VERSION=$1
+
+    git checkout master
+    git reset --hard HEAD
+    git tag "v${NEW_VERSION}" -m "${NEW_VERSION} release"
+
+    git push origin
+    git push origin --tags
+fi
