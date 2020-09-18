9f8e273e1a48856cdb7a48b3fa15051b8f6692b1
Brad Rogers <brad12rogers@gmail.com>
2020-09-17 17:50:20 -0700

    Move generate to ubuntu specific script

---
+++ generate-ubuntu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
+
+import glob
+import os
+import shutil
+import subprocess
+
+
+def main():
+    """The entry point for the program."""
+    
+    repo_path = 'dropbox-sdk-dotnet'
+    stone_path = os.path.abspath('stone')
+    print('Generating Stone types')
+    try:
+        shutil.rmtree(os.path.join(repo_path, 'Dropbox.Api', 'Generated'))
+    except OSError:
+        pass
+    try:
+        subprocess.check_output(
+            (['python', '-m', 'stone.cli', '--filter-by-route-attr', 'alpah_group=null', '-a:all', 'generator/csharp.stoneg.py'] +
+             [os.path.join(repo_path, 'Dropbox.Api')] + glob.glob('spec/*.stone')),
+            cwd=stone_path)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+if __name__ == '__main__':
+    main()
