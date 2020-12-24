93a016b7f5e2962b5204f6df55be30e57aaab5ea
Connor Worley <cworley@dropbox.com>
2020-12-23 16:11:49 -0800

    PR feedback

--- scripts/generate_stone.ps1
+++ scripts/generate_stone.ps1
@@ -5,7 +5,7 @@ if (!(Get-Command venv/bin/python -ErrorAction SilentlyContinue)) {
     python3 -m venv venv
 }
 if (!(Get-Command venv/bin/stone -ErrorAction SilentlyContinue)) {
-    venv/bin/pip install stone
+    venv/bin/pip install -r requirements-dev.txt
 }
 rm -rf dropbox-sdk-dotnet/Dropbox.Api/Generated
 echo "Generating Stone types"
