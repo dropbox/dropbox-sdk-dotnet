0a8e5b1cd7ecc7ae1bc897f9280eb0eb82a2b913
Brad Rogers <brad12rogers@gmail.com>
2021-02-19 14:12:27 -0800

    Fix Spec Update Action

--- generator/generate_routes.py
+++ generator/generate_routes.py
@@ -9,15 +9,10 @@ import subprocess
 
 def main():
     """The entry point for the program."""
-    stone_path = os.path.abspath('stone')
-    print(stone_path)
     generator_path = os.path.abspath('generator')
-    print(generator_path)
     spec_path = os.path.abspath('spec')
-    print(spec_path)
     specs = glob.glob(spec_path + '/*.stone')  # Arbitrary sorting
     specs.sort()
-    print(specs)
 
     repo_path = os.path.abspath('dropbox-sdk-dotnet')
     print('Generating Stone types')
@@ -28,8 +23,7 @@ def main():
     try:
         subprocess.check_output(
             (['python', '-m', 'stone.cli', '--filter-by-route-attr', 'alpah_group=null', '-a:all', generator_path + '/csharp.stoneg.py'] +
-             [os.path.join(repo_path, 'Dropbox.Api')] + specs),
-            cwd=stone_path)
+             [os.path.join(repo_path, 'Dropbox.Api')] + specs))
     except subprocess.CalledProcessError as e:
         print(e.output)
 
